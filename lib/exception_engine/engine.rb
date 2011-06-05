97ae81db23b9c33c61ff9497f0762f3af2d7c045
Grzesiek Kolodziejczyk <gkolodziejczyk@gmail.com>
2011-06-05 14:15:48 +0200

    Move mongoid config to an initializer, check if mongoid is already configured

--- lib/exception_engine/engine.rb
+++ lib/exception_engine/engine.rb
@@ -1,6 +1,18 @@
 module ExceptionEngine
   class Engine < Rails::Engine
     engine_name "exception_engine"
+
     config.app_middleware.use "ExceptionEngine::ExceptionMiddleware"
+
+    initializer "exception_engine_mongoid_initializer" do |app|
+      unless File.exist?(app.root.join('config', 'mongoid.yml'))
+        Mongoid.configure do |config|
+          name = "exception_engine-#{Rails.env}"
+          host = "localhost"
+          config.master = Mongo::Connection.new.db(name)
+          config.persist_in_safe_mode = false
+        end
+      end
+    end
   end
 end
